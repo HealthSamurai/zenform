6670f736c80f29b30ce42f6681480ad9e229e5d7
Aitem <muradbei@gmail.com>
2018-06-20 16:03:57 +0300

    add codemirror

--- src/zenform/core.cljs
+++ src/zenform/core.cljs
@@ -3,13 +3,15 @@
    [cljs.pprint :as pprint]
    [re-frame.core :as rf]
    [zenform.model :as model]
-   [zenform.inputs.core :as inputs]))
+   [zenform.inputs.core :as inputs]
+   [zenform.inputs.codemirror :as cm]))
 
 (def text-input #'inputs/text-input)
 (def invalid-feedback #'inputs/invalid-feedback)
 (def select #'inputs/select)
 (def toggle #'inputs/toggle)
 (def radio  #'inputs/radio)
+(def codemirror  #'cm/input)
 
 (defn form-value [form-path]
   (let [v (rf/subscribe [:zf/get-value form-path])]
@@ -23,9 +25,3 @@
 
 (defn pprint [v]
   [:pre (with-out-str (pprint/pprint v))])
-
-
-(comment
-
-  (js/console.log "here")
-  )
